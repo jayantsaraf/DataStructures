5e51cf9fcc20ce841ac8b99eb7dcca58e036f753
jayantsaraf <sarafjayant24@gmail.com>
2020-10-21 21:03:11 +0530

    [jayant]Add. initial commit

---
+++ LinkedList/LinkedList.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LinkedList
+{
+    public class LinkedList
+    {
+        internal Node head;
+        public void Add(int data)
+        {
+            Node node = new Node(data);
+            if(this.head==null)
+                this.head = node;
+            else
+            {
+                Node temp = head;
+                while(temp.next!=null)
+                    temp = temp.next;
+                temp.next = node;
+            }
+            Console.WriteLine("{0} Data inserted in Linked List ", node.data);
+        }
+    }
+}
