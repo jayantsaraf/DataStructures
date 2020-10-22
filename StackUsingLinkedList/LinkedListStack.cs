9d6fae741a45ac364408f93299f212e86a1dbec6
jayantsaraf <sarafjayant24@gmail.com>
2020-10-22 14:15:16 +0530

    [jayant]Add. added stack implementation and added 56->30->70

---
+++ StackUsingLinkedList/LinkedListStack.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StackUsingLinkedList
+{
+    class LinkedListStack
+    {
+        public Node head;
+        public void Push(int data)
+        {
+            Node node = new Node(data);
+            Node temp = this.head;
+            if(temp==null)
+                head.next = node;
+            else
+            {
+                node.next = temp;
+                head.next = node;
+            }
+        }
+        public void Display()
+        {
+            Node temp = this.head;
+            if(temp==null)
+            {
+                Console.WriteLine("Stack is empty");
+                return;
+            }
+            while(temp.next!=null)
+            {
+                Console.Write(temp.data + " ");
+                temp = temp.next;
+            }
+        }
+    }
+}
