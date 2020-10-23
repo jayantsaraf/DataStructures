008d446ed6b943ee9a38fa6b9dae9a7e472bc5e4
jayantsaraf <sarafjayant24@gmail.com>
2020-10-23 14:53:03 +0530

    [jayant]Add. added code to implement Binary Search Tree and added 56,30,70 to it

---
+++ BinarySearchTree/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace BinarySearchTree
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Binary Search Tree Implementation");
+            ImplementBST<int> bST = new ImplementBST<int>(56);
+            bST.InsertData(30);
+            bST.InsertData(70);
+            bST.Display();
+        }
+    }
+}
