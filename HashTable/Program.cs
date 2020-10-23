7bd1fcabeba28dce5c4e29ac566bae0dc3e668fb
jayantsaraf <sarafjayant24@gmail.com>
2020-10-23 16:14:13 +0530

    [jayant]Add. added project to implement hash table

--- HashTable/Program.cs
+++ HashTable/Program.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using HashTableDemo;
+using Microsoft.VisualBasic;
+using System;
 
 namespace HashTable
 {
@@ -7,6 +9,16 @@ namespace HashTable
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            MyMapNode<string, string> hash = new MyMapNode<string, string>(5);
+            hash.Add("0", "To");
+            hash.Add("1", "Be");
+            hash.Add("2", "Or");
+            hash.Add("3", "Not");
+            hash.Add("4", "To");
+            hash.Add("5", "Be");
+            string hash5 = hash.Get("5");
+            Console.WriteLine("Element present at 5th index:{0}",hash5);
+
         }
     }
 }
